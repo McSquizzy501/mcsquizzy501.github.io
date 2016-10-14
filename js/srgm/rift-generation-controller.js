f9fe8438fae606b81f611da95f0ed0ffe022dc51
McSquizzy501 <mcsquizzy.furrytail@gmail.com>
2016-10-14 10:28:44 -0400

    Placeholders
    Rift generation logic

---
+++ js/srgm/rift-generation-controller.js
@@ -0,0 +1,12 @@
+(function(){
+  var app = angular.module('rift-generation-module',[]);
+  
+  app.module('riftGenerationController',function(){
+    this.results = [];
+    
+    this.generateResults = function(numResults){};
+    this.generateResult = function(){};
+    
+  });
+  
+})();
