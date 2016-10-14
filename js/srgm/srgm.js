68d21b25f2196f63a618c9ef4e53fb086a8d6967
McSquizzy501 <mcsquizzy.furrytail@gmail.com>
2016-10-14 08:53:18 -0400

    Changing directions

---
+++ js/srgm/srgm.js
@@ -0,0 +1,30 @@
+(function() {
+  var app = angular.module('SRGM', []);
+
+  app.controller('NavBarController', function() {
+    this.navToggle = true;
+    this.dropDown1 = false;
+    this.dropDown2 = false;
+  });
+
+  app.controller('TabController', function() {
+    this.tab = 1;
+
+    this.setTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isActive = function(checkTab) {
+      return this.tab == checkTab;
+    };
+
+  });
+
+  app.directive('riftGeneration', function() {
+    return {
+      restrict: 'E',
+      template: "<h1>This is the Rift Generation section</h1>"
+    };
+  });
+
+})();
