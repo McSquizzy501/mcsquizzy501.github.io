129645c1a8fe9955ed88ae379593080a64e2f37e
McSquizzy501 <mcsquizzy.furrytail@gmail.com>
2016-10-14 11:14:21 -0400

    Update rift-generation-controller.js
    Testing key/value pairs

--- js/srgm/rift-generation-controller.js
+++ js/srgm/rift-generation-controller.js
@@ -1,12 +1,19 @@
 (function(){
   var app = angular.module('rift-generation-module',[]);
   
-  app.module('riftGenerationController',function(){
-    this.results = [];
+  app.controller('riftGenerationController',function(){
+    this.results = result;
+	this.text = "";
+	
+	$.each(results, function(k, v){
+		text = text+k+' = ' + v + '<br>';
+	});
     
     this.generateResults = function(numResults){};
     this.generateResult = function(){};
     
   });
+  
+  var result = $.parseJSON('json/rift-category.json')
   
 })();
