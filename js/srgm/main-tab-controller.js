74a0c1699d03d2120d3a370facdacc4e71cb638e
McSquizzy501 <mcsquizzy.furrytail@gmail.com>
2016-10-14 10:12:21 -0400

    Refactoring
    Moving main tab logic into its own controller

--- js/srgm/main-tab-controller.js
+++ js/srgm/main-tab-controller.js
@@ -1,1 +1,16 @@
 
+(function(){
+  var app = angular.module('main-tab-controller',[]);
+  app.controller('TabController', function() {
+    this.tab = 1;
+
+    this.setTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isActive = function(checkTab) {
+      return this.tab == checkTab;
+    };
+
+  });
+})();
