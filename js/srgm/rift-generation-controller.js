b6a2cacf8393c7b30a210265e192230652992a0e
McSquizzy501 <mcsquizzy.furrytail@gmail.com>
2016-10-18 13:50:55 -0400

    Update rift-generation-controller.js

--- js/srgm/rift-generation-controller.js
+++ js/srgm/rift-generation-controller.js
@@ -29,10 +29,18 @@
 		
 		this.loadJson('/js/srgm/json/rift-init-tables.json', function(data){
 			console.log("loadJson " + data);
+			for(var k in Object.keys(data))
+			{
+				console.log(key + ": " + data[key]);
+			}
 		});
 		
 		this.loadJson('/js/srgm/json/rift-tables.json', function(data){
 			console.log("loadJson " + data);
+			for(var k in Object.keys(data))
+			{
+				console.log(key + ": " + data[key]);
+			}
 		});
 		
 		this.generateResults = function(numResults){			
