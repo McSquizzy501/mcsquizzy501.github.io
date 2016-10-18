584839aa07b6dcf9913f82feab7c67bb6bebaee3
McSquizzy501 <mcsquizzy.furrytail@gmail.com>
2016-10-18 11:41:16 -0400

    Update rift-generation-controller.js

--- js/srgm/rift-generation-controller.js
+++ js/srgm/rift-generation-controller.js
@@ -8,24 +8,27 @@
 		
 		$http.get('/js/srgm/json/rift-tables.json').then(function (res){
 			self.tables = res;
+			console.log("Succesfully loaded tables: " + self.tables);
 		}, function (res){
 			console.log("Failed to load tables");
 		});
 			
 		$http.get('/js/srgm/json/rift-init-tables.json').then(function (res){
 			self.initial_tables = res;
+			console.log("Succesfully loaded initial tables: " + self.initial_tables);
 		}, function (res){
 			console.log("Failed to load initial tables");
 		});
 		
 		this.inputNumResults = 10;
-        this.test = myText;
+        	this.test = myText;
 		this.results = [];
 		
 		this.tables = {};
 		this.initial_tables = [];
 		
 		this.generateResults = function(numResults){
+			console.log("Generating " + numResults + " results");
 			for( var i = 0; i < numResults; i++)
 			{
 				this.generateResult();
