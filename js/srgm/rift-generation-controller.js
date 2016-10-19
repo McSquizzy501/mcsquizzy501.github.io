2900cc3bd36d8d70f2b2a43b027b9968145a6c6d
McSquizzy501 <mcsquizzy.furrytail@gmail.com>
2016-10-19 16:35:58 -0400

    Update rift-generation-controller.js

--- js/srgm/rift-generation-controller.js
+++ js/srgm/rift-generation-controller.js
@@ -6,6 +6,12 @@
     //this.results = result;
           var self = this;
     diceService.soundOff();
+    console.log(diceService.replaceInString("base: [1d6]"));
+    console.log(diceService.replaceInString("addition: [1d6+1]"));
+    console.log(diceService.replaceInString("subtraction: [1d6-1]"));
+    console.log(diceService.replaceInString("multiplication: [1d6*1]"));
+    console.log(diceService.replaceInString("division: [1d6/1]"));
+    console.log(diceService.replaceInString("multadd: [1d6*2+1]"));
     this.inputNumResults = 1;
           this.test = myText;
     this.rifts = [];    
@@ -120,13 +126,13 @@
           
           if( !(key in rift) )
           {
-            rift[key] = diceService.replaceInText(txt);
+            rift[key] = diceService.replaceInString(txt);
           }
           else
           {
             if (rift[key].constructor == Array)
             {
-              rift[key].push(diceService.replaceInText(txt));
+              rift[key].push(diceService.replaceInString(txt));
             }
             else
             {
