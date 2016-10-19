5eef97e7ae714dd5066c779a85900eaf9c60da09
McSquizzy501 <mcsquizzy.furrytail@gmail.com>
2016-10-19 15:28:41 -0400

    Update rift-generation-controller.js

--- js/srgm/rift-generation-controller.js
+++ js/srgm/rift-generation-controller.js
@@ -1,175 +1,48 @@
 (function(){
-  var app = angular.module('rift-generation-controller',['utilities']);
-
-	// Controllers
-	app.controller('riftGenerationController',['$http', function($http, diceService){
-		//this.results = result;
-        	var self = this;
-		diceService.soundOff();
-		this.inputNumResults = 1;
-        	this.test = myText;
-		this.rifts = [];		
+	var app = angular.module('utilities',[]);
 	
-		this.loadJson = function(url, callback){
-			
-			$http.get(url).then(
-				function (response){
-					callback(response);
-					console.log("Succesfully loaded " + url);
-				}, 
-				function (res){
-					console.log("Failed to load " + url);
-				}
-			);			
-		};
-		
-		this.loadJson('/js/srgm/json/rift-init-tables.json', function(response){
-			// console.log(response);
-			// console.log(response["data"]);			
-			self.initial_tables = response["data"]["tables"];
-			
-			// console.log(self.initial_tables);
-			
-		});
-		
-		this.loadJson('/js/srgm/json/rift-tables.json', function(response){
-			// console.log(response);
-			// console.log(response["data"]);			
-			self.tables = response["data"];
-			
-			// console.log(self.tables);
-			
-		});
-		
-		this.generateResults = function(numResults){			
-			console.log("Generating "+ numResults + " results");
-			
-			for(var i = 0; i < this.inputNumResults; i++)
-			{
-				console.log("Generating");
-				self.rifts.push(this.generateResult(i));
-			}
-			
-		};
-		
-		
-		this.generateResult = function(id){
-			// console.log(self.initial_tables);
-			var rift = {};
-			rift["id"] = id;
-			// var len = self.initial_tables.length;
-			// console.log("There are " + len + " initial tables;")
-			for(var i = 0; i < self.initial_tables.length; i++)
-			{	
-				var key = self.initial_tables[i];
-				console.log("key = " + key);
-				// console.log(self.tables);
-				// console.log(self.tables[key]);
-				this.processTable(key, rift);
-				console.log(rift);
-			}
-			
-			return rift;
-		};
-		
-		this.submit = function(){
-			this.generateResults(this.inputNumResults);
-		};
-		
-		this.processTable = function(key, rift)
-		{
-			//Entry is an object with dice and table attributes
-			//var obj = tables[entry]
-			console.log("processTable["+key+"]");
-			var entry = self.tables[key];
-			var dieSide = entry["dice"];
-			var roll = Math.floor((Math.random() * dieSide) + 1);
-			var table = entry["table"];
-			var found = false;
-			var index = 0;
-			var keys = Object.keys(table);
-			var txt = "";
-			
-			console.log("Roll: " + roll);
-			console.log(keys);
-			console.log(table);
-			
-			while(!found && index < keys.length)
-			{
-				var k = keys[index];
-				if(roll > k)
-				{
-					index++;
-				}
-				else
-				{
-					found = true;
-					var res = table[k];
-					var txt = "";
-					if(res.constructor == Array){
-						txt = res[0];
-						for(var i = 1; i < res.length; i++)
-						{
-							this.processTable(res[i], rift);
-						}
-					} else {
-						txt = res;
-					}
-					
-					console.log(key+" in rift? "+(key in rift));
-					
-					if( !(key in rift) )
-					{
-						rift[key] = txt;
-					}
-					else
-					{
-						if (rift[key].constructor == Array)
-						{
-							rift[key].push(txt);
-						}
-						else
-						{
-							var tmp = rift[key];
-							rift[key] = [];
-							rift[key].push(tmp);
-							rift[key].push(txt);
-						}
-					}
-					
-					console.log("Result["+key+"]: "+table[key]);
-				}
-			}
-		};
-		
-		// this.getMap(arry)
-		// {
-			// var map = new Map();
-			// for(int i = 0; i < arry.length; i++)
-			// {
-				// var obj = arry[i];
-				// map.set(Object.keys(obj)[0], obj[Object.keys(obj)[0]]);
+	app.service('diceservice', function(diceservice){
+    
+    this.version = '0.0.1';
+    
+    this.soundOff = function(){
+      console.log("[diceService] " + this.version);
+    };
+    
+    // this.replaceInString = function(string){
+			// var match = /\[(\d+)?d(\d+)([*/]\d+)*([+-]\d+)*?\]$/.exec(dieSpec);
+			// if (!match) {
+				// throw "Invalid dice notation: " + dieSpec;
 			// }
-			// return map;
+
+			// var howMany = (typeof match[1] == 'undefined') ? 1 : parseInt(match[1]);
+			// var dieSize = parseInt(match[2]);
+			// var modifiers = [];
+			// for(var i = 3; i < match.length; i++){
+				// if(typeof match[i] != 'undefined'))
+				// {
+					// modifiers.push(match[i]);
+				// }
+			// }
+			
+			// var sum = 0;
+			
+			// for( var i = 0; i < howMany; i++)
+			// {
+				// sum += Math.floor((Math.random() * dieSize) + 1);
+			// }
+			
+			// var str = ""+sum;
+			
+			// for( var i = 0; i < modifers.length; i++)
+			// {
+				// str = str+modifiers[i];
+			// }
+			
+			// sum = eval(str);
 		// }
-
+    
+    // return diceService;
 		
-	}]);
-
-	// Directives
-	app.directive("riftGeneration", function() {
-		return {
-			restrict: 'E',
-			//template: "<h1>rift generation</h1>"
-			templateUrl: "../../html/rift-generation.html"
-		};
 	});
-
-	// Globals
-
-	var myText = "TEST VALUE";
-	
-	
-
-
 })();
