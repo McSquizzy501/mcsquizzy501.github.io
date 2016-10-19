6258e9b85fe7902cc3807938756d8f4ad514cbd9
McSquizzy501 <mcsquizzy.furrytail@gmail.com>
2016-10-19 16:26:54 -0400

    Update utilities.js

--- js/srgm/utilities.js
+++ js/srgm/utilities.js
@@ -1,7 +1,7 @@
 (function(){
-	var app = angular.module('utilities',[]);
+  var app = angular.module('utilities',[]);
 	
-	app.service('diceService', function(){
+  app.service('diceService', function(){
     
     this.version = '0.0.1';
     
@@ -10,37 +10,39 @@
     };
     
     this.replaceInString = function(dieSpec){
-			var match = /[\w\s]*\[(\d+)?d(\d+)([*/]\d+)?([+-]\d+)?\][\w\s]*$/.exec(dieSpec);
-			if (!match) {
-				throw "Invalid dice notation: " + dieSpec;
-			}
+      var match = /[\w\s]*\[(\d+)?d(\d+)([*/]\d+)?([+-]\d+)?\][\w\s]*$/.exec(dieSpec);
+      if (!match) {
+        throw "Invalid dice notation: " + dieSpec;
+      }
 
-			var howMany = (typeof match[1] == 'undefined') ? 1 : parseInt(match[1]);
-			var dieSize = parseInt(match[2]);
-			var modifiers = [];
-			for(var i = 3; i < match.length; i++){
-				if(typeof match[i] != 'undefined')
-				{
-					modifiers.push(match[i]);
-				}
-			}
+      var howMany = (typeof match[1] == 'undefined') ? 1 : parseInt(match[1]);
+      var dieSize = parseInt(match[2]);
+      var modifiers = [];
+      for(var i = 3; i < match.length; i++){
+        if(typeof match[i] != 'undefined')
+        {
+          modifiers.push(match[i]);
+        }
+      }
 			
-			var sum = 0;
+      var sum = 0;
 			
-			for( var i = 0; i < howMany; i++)
-			{
-				sum += Math.floor((Math.random() * dieSize) + 1);
-			}
+      for( var i = 0; i < howMany; i++)
+      {
+        sum += Math.floor((Math.random() * dieSize) + 1);
+      }
 			
-			var str = ""+sum;
-			
-			for( var i = 0; i < modifiers.length; i++)
-			{
-				str = str+modifiers[i];
-			}
-			
-			return eval(str);
-		}
+      var str = ""+sum;
+
+      for( var i = 0; i < modifiers.length; i++)
+      {
+        str = str+modifiers[i];
+      }
+      
+      var val = eval(str);
+
+      return dieSpec.replace(/\[(\d+)?d(\d+)([*/]\d+)?([+-]\d+)?\]/, val);
+    }
 		
-	});
+  });
 })();
