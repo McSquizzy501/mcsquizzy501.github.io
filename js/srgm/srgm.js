c434b85374609332a7583860df59254e5089dd68
McSquizzy501 <mcsquizzy.furrytail@gmail.com>
2017-09-11 17:30:21 -0400

    Adding Travel Controller

--- js/srgm/srgm.js
+++ js/srgm/srgm.js
@@ -1,37 +1,29 @@
 (function() {
-  var app = angular.module('SRGM', []);
+  var app = angular.module('SRGM', ['main-tab-controller','navbar-controller', 'rift-generation-controller', 'travel-controller']);
 
-  app.controller('NavBarController', function() {
+  /*app.controller('NavBarController', function() {
     this.navToggle = true;
-    this.dropDown1 = false;
-    this.dropDown2 = false;
+    this.dropDown1 = true;
+    this.dropDown2 = true;
+    this.dropDowns = [false, false];
+    
+    this.isDdOpen = function(dd) {
+      return this.dropDowns[dd];
+    };
+    
+    this.toggleDd = function(dd) {
+      this.dropDowns[dd] = !this.dropDowns[dd];
+    };
+    
   });
-
   app.controller('TabController', function() {
     this.tab = 1;
-
     this.setTab = function(setTab) {
       this.tab = setTab;
     };
-
     this.isActive = function(checkTab) {
       return this.tab == checkTab;
     };
-
-  });
-
-  app.directive('riftGeneration', function() {
-    return {
-      restrict: 'E',
-      template: "<h1>This is the Rift Generation section</h1>"
-    };
-  });
-  
-  app.directive('travelGeneration', function() {
-    return {
-      restrict: 'E',
-      template: "<h1>This is the Rift Generation section</h1>"
-    };
-  });
+  });*/
 
 })();
